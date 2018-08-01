366ff09ded6af9519d273fb50bceb2cae58282ac
ChinaMobile <574469551@qq.com>
2018-08-01 14:17:24 +0800

    ts 学习

--- test/test.js
+++ test/test.js
@@ -1,44 +1,37 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Control = /** @class */ (function () {
-    function Control() {
+var Validation;
+(function (Validation) {
+    var lettersRegexp = /^[A-Za-z]+$/;
+    var numberRegexp = /^[0-9]+$/;
+    var LettersOnlyValidator = /** @class */ (function () {
+        function LettersOnlyValidator() {
+        }
+        LettersOnlyValidator.prototype.isAcceptable = function (s) {
+            return lettersRegexp.test(s);
+        };
+        return LettersOnlyValidator;
+    }());
+    Validation.LettersOnlyValidator = LettersOnlyValidator;
+    var ZipCodeValidator = /** @class */ (function () {
+        function ZipCodeValidator() {
+        }
+        ZipCodeValidator.prototype.isAcceptable = function (s) {
+            return s.length === 5 && numberRegexp.test(s);
+        };
+        return ZipCodeValidator;
+    }());
+    Validation.ZipCodeValidator = ZipCodeValidator;
+})(Validation || (Validation = {}));
+// Some samples to try
+var strings = ["Hello", "98052", "101"];
+// Validators to use
+var validators = {};
+validators["ZIP code"] = new Validation.ZipCodeValidator();
+validators["Letters only"] = new Validation.LettersOnlyValidator();
+// Show whether each string passed each validator
+for (var _i = 0, strings_1 = strings; _i < strings_1.length; _i++) {
+    var s = strings_1[_i];
+    for (var name_1 in validators) {
+        console.log("\"" + s + "\" - " + (validators[name_1].isAcceptable(s) ? "matches" : "does not match") + " " + name_1);
     }
-    return Control;
-}());
-var Button = /** @class */ (function (_super) {
-    __extends(Button, _super);
-    function Button() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    Button.prototype.select = function () { };
-    return Button;
-}(Control));
-var TextBox = /** @class */ (function (_super) {
-    __extends(TextBox, _super);
-    function TextBox() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    TextBox.prototype.select = function () { };
-    return TextBox;
-}(Control));
-// 错误：“Image”类型缺少“state”属性。
-var Image = /** @class */ (function () {
-    function Image() {
-    }
-    Image.prototype.select = function () { };
-    return Image;
-}());
-var Location = /** @class */ (function () {
-    function Location() {
-    }
-    return Location;
-}());
+}
--- test/test.ts
+++ test/test.ts
@@ -1,24 +1,35 @@
-class Control {
-    private state: any;
+namespace Validation {
+    export interface StringValidator {
+        isAcceptable(s: string): boolean;
+    }
+
+    const lettersRegexp = /^[A-Za-z]+$/;
+    const numberRegexp = /^[0-9]+$/;
+
+    export class LettersOnlyValidator implements StringValidator {
+        isAcceptable(s: string) {
+            return lettersRegexp.test(s);
+        }
+    }
+
+    export class ZipCodeValidator implements StringValidator {
+        isAcceptable(s: string) {
+            return s.length === 5 && numberRegexp.test(s);
+        }
+    }
 }
 
-interface SelectableControl extends Control {
-    select(): void;
-}
+// Some samples to try
+let strings = ["Hello", "98052", "101"];
 
-class Button extends Control implements SelectableControl {
-    select() { }
-}
+// Validators to use
+let validators: { [s: string]: Validation.StringValidator; } = {};
+validators["ZIP code"] = new Validation.ZipCodeValidator();
+validators["Letters only"] = new Validation.LettersOnlyValidator();
 
-class TextBox extends Control {
-    select() { }
-}
-
-// 错误：“Image”类型缺少“state”属性。
-class Image implements SelectableControl {
-    select() { }
-}
-
-class Location {
-
+// Show whether each string passed each validator
+for (let s of strings) {
+    for (let name in validators) {
+        console.log(`"${ s }" - ${ validators[name].isAcceptable(s) ? "matches" : "does not match" } ${ name }`);
+    }
 }
