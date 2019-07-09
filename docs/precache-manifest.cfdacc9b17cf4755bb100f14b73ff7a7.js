8e390169e11c2d29b223b4097a06f9ddf90ca3a8
谭健青 <574469551@qq.com>
2019-07-09 12:17:46 +0800

    init

---
+++ docs/precache-manifest.cfdacc9b17cf4755bb100f14b73ff7a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "43647b5b83a54368f688",
+    "url": "js/chunk-vendors.d207454b.js"
+  },
+  {
+    "revision": "2b014de57c4afe239d5b",
+    "url": "js/app.b4103462.js"
+  },
+  {
+    "revision": "36db3b639d647d77c593",
+    "url": "js/about.bb8d2f4c.js"
+  },
+  {
+    "revision": "33e578f183e130eb1bc56fde4edda53a",
+    "url": "index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2b014de57c4afe239d5b",
+    "url": "css/app.2fbafe3b.css"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9e07290559f44634468e1cdc83c84fc4.js"
+  "precache-manifest.cfdacc9b17cf4755bb100f14b73ff7a7.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "hello-world"});
