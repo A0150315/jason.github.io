e1089b5c9d501be5d858aa418aa7e0c5825a68d4
谭健青 <574469551@qq.com>
2019-07-09 12:55:58 +0800

    init

---
+++ docs/precache-manifest.e955b6517f6914086b4ce1c2afd15a3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Jason.github.io/robots.txt"
+  },
+  {
+    "revision": "43647b5b83a54368f688",
+    "url": "/Jason.github.io/js/chunk-vendors.d207454b.js"
+  },
+  {
+    "revision": "cacf49b89c24d3d81a6f",
+    "url": "/Jason.github.io/js/app.a6c61064.js"
+  },
+  {
+    "revision": "36db3b639d647d77c593",
+    "url": "/Jason.github.io/js/about.bb8d2f4c.js"
+  },
+  {
+    "revision": "72e9b2a2a9ff2b0cc2302016bed44b0a",
+    "url": "/Jason.github.io/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/Jason.github.io/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "cacf49b89c24d3d81a6f",
+    "url": "/Jason.github.io/css/app.2fbafe3b.css"
+  }
+];
