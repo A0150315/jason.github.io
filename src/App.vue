cdeaed3c799cacfdff5cc80a7615f2f3b6d1d6d2
谭健青 <574469551@qq.com>
2019-07-10 14:53:23 +0800

    chore:新增全局scss

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,15 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <div class="test">1</div>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
