f128e0af63a1a05746618cf174631d2143c71c21
谭健青 <574469551@qq.com>
2019-12-11 12:45:11 +0800

    hotfix:修复公众号导致的图片参数不一致

--- src/views/GenerateSVG/index.vue
+++ src/views/GenerateSVG/index.vue
@@ -35,20 +35,22 @@ export default Vue.extend({
   computed: {
     clipboardText() {
       const rate = (this.height / this.width * 100).toFixed(2)
+      const backgroundUrl = this.backgroundUrl.split('?')[0];
+      const coverUrl = this.coverUrl.split('?')[0];
       return (
         `<p><br></p>
 <p style="max-height: 0;min-height: 0;overflow: hidden;">
-  <img class="rich_pages" data-ratio="1.16640625" data-s="300,640" src="${this.backgroundUrl}" data-type="jpeg" data-w="1280" style="display:block;height:0 !important" _src="${this.backgroundUrl}">
+  <img class="rich_pages" data-ratio="1.16640625" data-s="300,640" src="${backgroundUrl}" data-type="jpeg" data-w="1280" style="display:block;height:0 !important" _src="${backgroundUrl}">
 </p>
 <p style="max-height: 0;min-height: 0;overflow: hidden;">
-  <img class="rich_pages" data-ratio="1.16640625" data-s="300,640" src="${this.coverUrl}" data-type="jpeg" data-w="1280" style="display:block;height:0 !important" _src="${this.coverUrl}">
+  <img class="rich_pages" data-ratio="1.16640625" data-s="300,640" src="${coverUrl}" data-type="jpeg" data-w="1280" style="display:block;height:0 !important" _src="${coverUrl}">
 </p>
 <section style="text-align: justify;display: inline-block;">
   <section style="display: inline-block;vertical-align: middle;line-height: 0;box-sizing: border-box;width: ${this.width}px;">
-    <section style="background-image: url(&quot;${this.backgroundUrl}&quot;);background-position: 50% 50%;background-repeat: no-repeat;background-size: cover;background-attachment: scroll;width: 100%;margin-left: auto;margin-right: auto;box-sizing: border-box;height: 0;padding-bottom: ${rate}%;">
+    <section style="background-image: url(&quot;${backgroundUrl}&quot;);background-position: 50% 50%;background-repeat: no-repeat;background-size: cover;background-attachment: scroll;width: 100%;margin-left: auto;margin-right: auto;box-sizing: border-box;height: 0;padding-bottom: ${rate}%;">
     </section>
     <section style="box-sizing: border-box;margin-top: -${rate}%;height: 0;padding-bottom:  ${rate}%;">
-      <svg style="background-image: url(&quot;${this.coverUrl}&quot;);background-position: 50% 50%;background-repeat: no-repeat;background-size: cover;background-attachment: scroll;width: 100%;margin-left: auto;margin-right: auto;box-sizing: border-box;height: 0;padding-bottom:${rate}%;" xmlns="http://www.w3.org/2000/svg" width="1">
+      <svg style="background-image: url(&quot;${coverUrl}&quot;);background-position: 50% 50%;background-repeat: no-repeat;background-size: cover;background-attachment: scroll;width: 100%;margin-left: auto;margin-right: auto;box-sizing: border-box;height: 0;padding-bottom:${rate}%;" xmlns="http://www.w3.org/2000/svg" width="1">
         <animate attributeName="width" style="box-sizing: border-box;" fill="freeze" to="0" from="1" duration="0.01" begin="click + 0.01s">
         </animate>
         <animate attributeName="opacity" style="box-sizing: border-box;" fill="freeze" dur="0.01" begin="click" from="1" to="0">
