883cebb5d38d50f8af4ea17887dbdbe00878e9ce
谭健青 <574469551@qq.com>
2019-12-11 10:38:06 +0800

    publish

--- docs/precache-manifest.1fddc082d222fe8802fe0372a79e1e34.js
+++ docs/precache-manifest.1fddc082d222fe8802fe0372a79e1e34.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/Jason.github.io/js/chunk-vendors.59f38e6f.js"
   },
   {
-    "revision": "1522a157a0808fc65777",
-    "url": "/Jason.github.io/js/app.3a12c5c5.js"
+    "revision": "f56302eee83fe24acbcc",
+    "url": "/Jason.github.io/js/app.39899f7e.js"
   },
   {
     "revision": "866d1479bae56345f485",
     "url": "/Jason.github.io/js/about.8a0cb839.js"
   },
   {
-    "revision": "45eced40780d0b3d354aba3225f4a292",
+    "revision": "a064846809aba596898c70f469556e4c",
     "url": "/Jason.github.io/index.html"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/Jason.github.io/img/state.2757ee59.png"
   },
   {
-    "revision": "1522a157a0808fc65777",
+    "revision": "f56302eee83fe24acbcc",
     "url": "/Jason.github.io/css/app.103c3460.css"
   }
 ];
