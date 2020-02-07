0e036fe78fa3297fe1ddd03be1ee90b029da9a3d
JasonTan <574469551@qq.com>
2020-02-07 22:36:42 +0800

    Feat:新增脚本

---
+++ src/assets/blogs-data.ts
@@ -0,0 +1,8 @@
+export default [
+  { divider: true },
+  { heading: 'Blogs' },
+  { icon: 'mdi-@', text: 'Article 1', to: '/blogs' },
+  { icon: 'mdi-@', text: 'Article 2' },
+  { icon: 'mdi-@', text: 'Article 3' },
+  { icon: 'mdi-@', text: 'Article 4' }
+];
