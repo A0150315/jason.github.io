e641e871de4b8b2edaca4fdd01a51eb862322f29
JasonTan <574469551@qq.com>
2020-02-12 18:16:24 +0800

    init node

--- scripts/build-directory.ts
+++ scripts/build-directory.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
-fs.writeFile('./message.txt', '第一行\r\n第1行', function(err) {
+fs.writeFile('./src/assets/blog-data.tsx', '第一行\r\n第1行', function(err) {
   if (err) console.log(1);
   else console.log('写文件操作成功');
 });
